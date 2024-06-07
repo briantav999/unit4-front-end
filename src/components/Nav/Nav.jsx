0af94896461add17608b303e90dcaffc04cbe793
“Karlon <karlon.heileman@gmail.com>
2024-06-07 13:21:12 -0500

    Nav & services

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,31 @@
-const Nav = () => {
-    return <>
+// src/components/Nav.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css';
 
-    </>
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <h1>Music Instrument Inventory</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/instruments">Instruments</Link>
+                </li>
+                <li>
+                    <Link to="/create">Add Instrument</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
-export default Nav
+export default Nav;
---
+++ src/services/instrumentService.js
@@ -0,0 +1,71 @@
+// src/components/services/instrumentService.js
+
+const API_URL = 'http://localhost:8000/api/instruments/'; // Update the URL based on your Django API endpoint
+
+const fetchInstruments = async () => {
+    try {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const fetchInstrument = async (id) => {
+    try {
+        const res = await fetch(`${API_URL}${id}/`);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const createInstrument = async (formData) => {
+    try {
+        const res = await fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const updateInstrument = async (formData, id) => {
+    try {
+        const res = await fetch(`${API_URL}${id}/`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const deleteInstrument = async (id) => {
+    try {
+        const res = await fetch(`${API_URL}${id}/`, {
+            method: 'DELETE',
+        });
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default {
+    fetchInstruments,
+    fetchInstrument,
+    createInstrument,
+    updateInstrument,
+    deleteInstrument,
+};
