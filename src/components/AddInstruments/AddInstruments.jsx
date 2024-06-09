4be837fb1e96d8fd2bbe0e7bccc9880a8ee05306
jsangalang93 <jsangalang93@gmail.com>
2024-06-09 11:39:13 -0400

    create works

--- src/components/AddInstruments/AddInstruments.jsx
+++ src/components/AddInstruments/AddInstruments.jsx
@@ -1,29 +1,39 @@
 import { useState } from 'react'
 import './AddInstruments.css'
+import { createInstrument } from '../../services/instrumentService'
 
-const [Instrument, setInstrument] = useState([]);
-const [isCreateOpen, setIsCreateOpen] = useState(false);
-const [newInstrument, setNewInstrument] = useState([]);
 
-const handleAddInstrument = async (formData) => {
-    try{
-      const newInstrument = await createInstrument(formData);
-      setInstruments([...Instruments, NewInstrument]);
-      setIsCreateOpen(false);
-    }catch(error){
-      console.error(error);
+
+const Create = () => {
+    const [Instruments, setInstruments] = useState([]);
+    const [isCreateOpen, setIsCreateOpen] = useState(false);
+    const [newInstrument, setNewInstrument] = useState([]);
+    
+    const handleAddInstrument = async (formData) => {
+        try{
+            // console.log(formData)
+            const newInstrument = await createInstrument(formData);
+            setInstruments([...Instruments, newInstrument]);
+            setIsCreateOpen(false);
+
+        }catch(error){
+            console.error(error);
+        }
     }
-  }
-  
 
-const Create = ({handleAddInstrument}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleAddInstrument(newInstrument);
+    }
+  
+   
     const [Instrument, setInstrument] = useState({
         name: '',
         category: '',
         brand: '',
         model: '',
         condition: '',
-        isRented: false,
+        isRented: false
 
     })
 
@@ -48,23 +58,30 @@ return(
             e.preventDefault()
             handleAddInstrument(Instrument)
         }}>
-            <label>Name:</label>
-            <input type="text" name="name" id="name" maxlength="32" value={Instrument.name} onChange={handleChange} />
-            <label>Category:</label>
-            <input type="text" name="category" id="category" maxlength="32" value={Instrument.category} onChange={handleChange} />
-            <label>Brand:</label>
-            <input type="text" name="brand" id="brand" maxlength="32" value={Instrument.brand} onChange={handleChange} />
-            <label>Model:</label>
-            <input type="text" name="model" id="model" maxlength="32" value={Instrument.model} onChange={handleChange} />
-            <label>Condition:</label>
-            <input type="text" name="condition" id="condition" maxlength="32" value={Instrument.condition} onChange={handleChange} />
-            <label>isRented:</label>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" maxLength="32" value={Instrument.name} onChange={handleChange} />
+
+            <label htmlFor="category">Category:</label>
+            <input type="text" name="category" id="category" maxLength="32" value={Instrument.category} onChange={handleChange} />
+
+            <label htmlFor="brand">Brand:</label>
+            <input type="text" name="brand" id="brand" maxLength="32" value={Instrument.brand} onChange={handleChange} />
+
+            <label htmlFor="model">Model:</label>
+            <input type="text" name="model" id="model" maxLength="32" value={Instrument.model} onChange={handleChange} />
+
+            <label htmlFor="condition">Condition:</label>
+            <input type="text" name="condition" id="condition" maxLength="32" value={Instrument.condition} onChange={handleChange} />
+
+            <label htmlFor="isRented">isRented:</label>
             <input type="checkbox" name="isRented" id="isRented" checked={Instrument.isRented} onChange={handleIsRented} />
+
             <button type="submit">Add Instrument</button>
             <button id="home">Return to Home</button>
         </form>
     </div>
 )
 }
+
 
 export default Create;
