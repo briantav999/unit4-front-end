106b81e43e8ce111ced18d2412ecc67b8642c12d
“Karlon <karlon.heileman@gmail.com>
2024-06-09 18:38:39 -0500

    updated route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import Nav from './components/Nav/Nav.jsx'
-import InventoryCard from './components/Home/InventoryCard/InventoryCard.jsx'
-import './App.css'
-import Create from './components/AddInstruments/AddInstruments.jsx'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav.jsx';
+import InventoryCard from './components/Home/InventoryCard/InventoryCard.jsx';
+import Create from './components/AddInstruments/AddInstruments.jsx';
+import './App.css';
 
 function App() {
-  
-  return <>
-    <Nav />
-    <h1>Instrument Rental Management</h1>
-    {/* <InventoryCard /> */}
-    <Create />
-  </>
+  return (
+    <>
+      <Nav />
+      <h1>Instrument Rental Management</h1>
+      <Routes>
+        <Route path="/" element={<InventoryCard />} />
+        <Route path="/create" element={<Create />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </>
+  );
 }
 
-
-export default App
+export default App;
