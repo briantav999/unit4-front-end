247faa2b462fba547a7187728adbfffc277acead
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-09 19:55:44 -0500

    search bar cleanup

--- src/components/Home/InventoryCard/SearchCard/SearchCard.jsx
+++ src/components/Home/InventoryCard/SearchCard/SearchCard.jsx
@@ -3,31 +3,18 @@ import './SearchCard.css'
 
 const SearchCard = () => {
     const [searchBar, setSearchBar] = useState('')
-    const [searchInstrument, setSearchInstrument] = useState([])
     
     // Func changes searchBar value as user types
     const handleSearchBar = (event) => {
         setSearchBar(event.target.value)
     }
 
-    // Func pulls req from api and displays filtered data
-    const handleSearchButton = async () => {
-        const URL = `http://localhost:5173/instruments/?search=${searchBar}`
-        const res = await fetch(URL);
-        console.log(URL);
-        const returnData = await res.json();
-        console.log(returnData);
-        setSearchInstrument(returnData);
-    }
-    
     return <>
         <div id="search-card">
             <input type="text" id='search-bar' 
                 placeholder='Search instruments'
                 value={searchBar}
                 onChange={handleSearchBar} />
-            <input type="button" id='search-button'
-                value="Search" onClick={handleSearchButton} />
         </div>
     </>
 }
