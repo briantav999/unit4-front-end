363176e66adae344b7a7d674240a83c574951e4d
“Karlon <karlon.heileman@gmail.com>
2024-06-09 20:04:35 -0500

    Up Sear jsx

--- src/components/Home/InventoryCard/SearchCard/SearchCard.jsx
+++ src/components/Home/InventoryCard/SearchCard/SearchCard.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react';
-import './SearchCard.css';
+import { useState } from 'react'
+import './SearchCard.css'
 
-const SearchCard = ({ onSearch }) => {
-    const [searchBar, setSearchBar] = useState('');
-
+const SearchCard = () => {
+    const [searchBar, setSearchBar] = useState('')
+    
     // Func changes searchBar value as user types
     const handleSearchBar = (event) => {
-        const value = event.target.value;
-        setSearchBar(value);
-        onSearch(value); // Pass search value to parent component
-    };
+        setSearchBar(event.target.value)
+    }
 
-    return (
+    return <>
         <div id="search-card">
-            <input 
-                type="text" 
-                id="search-bar" 
-                placeholder="Search instruments"
+            <input type="text" id='search-bar' 
+                placeholder='Search instruments'
                 value={searchBar}
                 onChange={handleSearchBar} />
-            <input type="button" id='search-button'
-                value="Search" onClick={handleSearchButton} />
         </div>
-    );
+    </>
 }
 
-export default SearchCard;
+export default SearchCard
